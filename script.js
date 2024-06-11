ab4408b8ba2bd1479c557ab36f4ee2845e3500f4
nico-frey <nico@freyy.ch>
2024-06-11 11:55:31 +0200

    feat: add example code, add image fetching

--- script.js
+++ script.js
@@ -1,1 +1,95 @@
-console.log("Hello, World!");
+const SPOTIFY_CLIENT_ID = "67b411e20d594f30bf7a8d3bbde54285";
+const SPOTIFY_CLIENT_SECRET = "161fc5e3df004b95af3ba8c62f3eaf54";
+const PLAYLIST_ID = "7fXKDSXrj7RljWC4QTixrd";
+const container = document.querySelector('div[data-js="tracks"]');
+
+//Get playlist from Spotify
+
+function fetchPlaylist(token, playlistId) {
+  console.log("token: ", token);
+
+  fetch(`https://api.spotify.com/v1/playlists/${playlistId}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+
+      if (data.tracks && data.tracks.items) {
+        data.tracks.items.forEach((item) => {
+          console.log(item.track.name);
+        });
+
+        addTracksToPage(data.tracks.items);
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+//Add tracks to HTML
+
+function addTracksToPage(items) {
+  if (!container) {
+    console.error("Container element not found");
+    return;
+  }
+
+  const ul = document.createElement("ul");
+
+  items.forEach((item) => {
+    console.log("track: ", item.track);
+    const li = document.createElement("li");
+
+    const coverImage =
+      item.track.album.images.length > 0 ? item.track.album.images[0].url : "";
+
+    // Add items to innerHTML
+
+    li.innerHTML = `
+    <p>${item.track.name} by ${item.track.artists
+      .map((artist) => artist.name)
+      .join(", ")}</p>
+    ${coverImage ? `<img src="${coverImage}" class="coverImage" >` : ""}
+  `;
+
+    ul.appendChild(li);
+  });
+  container.appendChild(ul);
+}
+
+function fetchAccessToken() {
+  fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: `grant_type=client_credentials&client_id=${SPOTIFY_CLIENT_ID}&client_secret=${SPOTIFY_CLIENT_SECRET}`,
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      if (data.access_token) {
+        fetchPlaylist(data.access_token, PLAYLIST_ID);
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+fetchAccessToken();
