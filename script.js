5a7111e6f196860b209f47fa538dde4ffd2a1661
nico-frey <nico@freyy.ch>
2024-06-11 12:29:49 +0200

    feat: add track lenght

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const SPOTIFY_CLIENT_ID = "67b411e20d594f30bf7a8d3bbde54285";
 const SPOTIFY_CLIENT_SECRET = "161fc5e3df004b95af3ba8c62f3eaf54";
-const PLAYLIST_ID = "7fXKDSXrj7RljWC4QTixrd";
+const PLAYLIST_ID = "5PsJRJZ4Ia7bjiY8EbieLd?si=0cbf8571fb38462a";
 const container = document.querySelector('div[data-js="tracks"]');
 
 //Get playlist from Spotify
@@ -46,19 +46,32 @@ function addTracksToPage(items) {
 
   const ul = document.createElement("ul");
 
+  // Looping through plalist
+
   items.forEach((item) => {
     console.log("track: ", item.track);
     const li = document.createElement("li");
 
+    // Get coverImage from Track obj
+
     const coverImage =
       item.track.album.images.length > 0 ? item.track.album.images[0].url : "";
+
+    // Get trackDuration from Track obj
+
+    const durationMs = item.track.duration_ms;
+    const minutes = Math.floor(durationMs / 60000);
+    const seconds = ((durationMs % 60000) / 1000).toFixed(0);
+    //console.log("track duration:", durationMs);
 
     // Add items to innerHTML
 
     li.innerHTML = `
-    <p>${item.track.name} by ${item.track.artists
+    <p class="trackName">${item.track.name}</p>  
+    <p class="artistName">${item.track.artists
       .map((artist) => artist.name)
       .join(", ")}</p>
+    <p>${minutes}:${seconds < 10 ? "0" : ""}${seconds}</p>
     ${coverImage ? `<img src="${coverImage}" class="coverImage" >` : ""}
   `;
 
