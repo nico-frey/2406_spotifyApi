95c3f0d4be3d1dd112f201bb01071d34b9e95b1f
nico-frey <nico@freyy.ch>
2024-06-12 02:19:25 +0200

    fix: finally made webpack work at 2:19am wtf

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var path = require("path");
+
+module.exports = {
+  mode: "development", // Change "development" to "production" or "none" as needed
+  entry: "./src/script.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/dist/",
+  },
+  devServer: {
+    static: path.join(__dirname),
+    port: 8080,
+    open: true,
+  },
+};
