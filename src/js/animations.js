7705a666e95df9c59f78fac1918a1b6b02b5b91e
nico-frey <nico@freyy.ch>
2024-06-13 21:10:13 +0200

    feat: Tweaking GSAP animation properties

--- src/js/animations.js
+++ src/js/animations.js
@@ -1,23 +1,86 @@
 import { gsap } from "gsap";
 
 export function toggleOn(element) {
-  gsap.to(element, {
-    duration: 0.5,
-    backgroundColor: "#E6EDF2",
-    color: "#e0e0ef",
-    ease: "power2.out",
-    scale: 5,
-  });
+  const image = element.querySelector(".coverImage");
+  const trackName = element.querySelector(".trackName");
+  const artistName = element.querySelector(".artistName");
+  const currentTrack = element.querySelector(".track");
+
+  gsap
+    .timeline()
+    .to(element, {
+      duration: 0.5,
+      backgroundColor: "#E6EDF2",
+      ease: "power4.out",
+      padding: "2rem",
+    })
+    .to(
+      image,
+      {
+        height: "10rem",
+        width: "10rem",
+        duration: 0.5,
+        scale: 1.1,
+        ease: "power2.out",
+      },
+      "<"
+    )
+    .to(
+      trackName,
+      {
+        fontSize: "2rem",
+        duration: 0.5,
+        color: "#057882",
+        ease: "power2.out",
+      },
+      "<"
+    )
+    .to(
+      artistName,
+      { duration: 0.5, color: "#057882", ease: "power2.out" },
+      "<"
+    );
 }
 
 export function toggleOff(element) {
-  gsap.to(element, {
-    duration: 0.5,
-    backgroundColor: "#FAFCFC",
-    color: "#798E90",
-    ease: "power2.out",
-    scale: 1,
-  });
+  const image = element.querySelector(".coverImage");
+  const trackName = element.querySelector(".trackName");
+  const artistName = element.querySelector(".artistName");
+
+  gsap
+    .timeline()
+    .to(element, {
+      duration: 0.5,
+      backgroundColor: "#FAFCFC",
+      ease: "power4.out",
+      padding: "0.5rem",
+    })
+    .to(
+      image,
+      {
+        height: "4rem",
+        width: "4rem",
+        duration: 0.5,
+        scale: 1,
+        ease: "power2.out",
+      },
+      "<"
+    )
+    .to(
+      trackName,
+      {
+        fontSize: "1.375rem",
+        duration: 0.5,
+        color: "#03191b",
+        ease: "power2.out",
+      },
+      "<"
+    )
+    .to(
+      artistName,
+      { duration: 0.5, color: "#798e90", ease: "power2.out" },
+      "<"
+    );
 }
 
 export function gsapAnimations() {
